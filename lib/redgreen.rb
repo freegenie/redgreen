9414253201c6698b8737bf0c75bb210e0071826f
Fabrizio Regini <freegenie@gmail.com>
2009-05-28 23:55:41 +0200

    Fixed issue with test info beeing shown in non testing rake commands.

--- lib/redgreen.rb
+++ lib/redgreen.rb
@@ -1,70 +1,79 @@
-require 'test/unit'
-require 'test/unit/ui/console/testrunner'
+if ARGV.size > 0 and ARGV[0].include? 'test'  
+  require 'test/unit'
+  require 'test/unit/ui/console/testrunner'
 
-# cute.
-module RedGreen
-  module Color
-    # 37 = white, 30 = black
-    FG_COLORS = { :for_clear => 37, :for_red => 37, :for_green => 37, :for_yellow => 30}   
-    BG_COLORS = { :clear => 0, :red => 41, :green => 42, :yellow => 43 }
-    def self.method_missing(color_name, *args)
-      color(color_name) + args.first + color(:clear) 
-    end
-    def self.color(color)
-      fg_color = FG_COLORS["for_#{color}".to_sym]
-      bg_color = BG_COLORS[color.to_sym]
-      "\e[#{fg_color};#{bg_color}m" 
-    end
-  end
-end
-
-class Test::Unit::UI::Console::RedGreenTestRunner < Test::Unit::UI::Console::TestRunner
-  def initialize(suite, output_level=NORMAL, io=$stdout)
-    super
-  end
-  
-  def output_single(something, level=NORMAL)
-    return unless (output?(level))
-    something = case something
-                when '.' then RedGreen::Color.green('.')
-                when 'F' then RedGreen::Color.red("F")
-                when 'E' then RedGreen::Color.yellow("E")
-                else something
-                end
-    @io.write(something) 
-    @io.flush
-  end
-end
-
-class Test::Unit::AutoRunner
-  alias :old_initialize :initialize
-  def initialize(standalone)
-    old_initialize(standalone)
-    @runner = proc do |r| 
-      Test::Unit::UI::Console::RedGreenTestRunner
+  # cute.
+  module RedGreen
+    module Color
+      # # 37 = white, 30 = black
+      #      FG_COLORS = { :for_clear => 37, :for_red => 37, :for_green => 37, :for_yellow => 30}
+      #      BG_COLORS = { :clear => 0, :red => 41, :green => 42, :yellow => 43 }
+      #      def self.method_missing(color_name, *args)
+      #        color(color_name) + args.first + color(:clear)
+      #      end
+      #      def self.color(color)
+      #        fg_color = FG_COLORS["for_#{color}".to_sym]
+      #        bg_color = BG_COLORS[color.to_sym]
+      #        "\e[#{fg_color};#{bg_color}m"
+      #      end
+      COLORS = { :clear => 0, :red => 31, :green => 32, :yellow => 33 }
+       def self.method_missing(color_name, *args)
+         color(color_name) + args.first + color(:clear)
+       end
+       def self.color(color)
+         "\e[#{COLORS[color.to_sym]}m"
+       end      
     end
   end
-end
 
-class Test::Unit::TestResult
-  alias :old_to_s :to_s
-  def to_s
-    if old_to_s =~ /\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors/
-      RedGreen::Color.send($1.to_i != 0 || $2.to_i != 0 ? :red : :green, $&)
+  class Test::Unit::UI::Console::RedGreenTestRunner < Test::Unit::UI::Console::TestRunner
+    def initialize(suite, output_level=NORMAL, io=$stdout)
+      super
+    end
+
+    def output_single(something, level=NORMAL)
+      return unless (output?(level))
+      something = case something
+      when '.' then RedGreen::Color.green('.')
+      when 'F' then RedGreen::Color.red("F")
+      when 'E' then RedGreen::Color.yellow("E")
+      else something
+      end
+      @io.write(something)
+      @io.flush
     end
   end
-end
 
-class Test::Unit::Failure
-  alias :old_long_display :long_display
-  def long_display
-    old_long_display.sub('Failure', RedGreen::Color.red('Failure'))
+  class Test::Unit::AutoRunner
+    alias :old_initialize :initialize
+    def initialize(standalone)
+      old_initialize(standalone)
+      @runner = proc do |r|
+        Test::Unit::UI::Console::RedGreenTestRunner
+      end
+    end
   end
-end
 
-class Test::Unit::Error
-  alias :old_long_display :long_display
-  def long_display
-    old_long_display.sub('Error', RedGreen::Color.yellow('Error'))
+  class Test::Unit::TestResult
+    alias :old_to_s :to_s
+    def to_s
+      if old_to_s =~ /\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors/
+        RedGreen::Color.send($1.to_i != 0 || $2.to_i != 0 ? :red : :green, $&)
+      end
+    end
+  end
+
+  class Test::Unit::Failure
+    alias :old_long_display :long_display
+    def long_display
+      old_long_display.sub('Failure', RedGreen::Color.red('Failure'))
+    end
+  end
+
+  class Test::Unit::Error
+    alias :old_long_display :long_display
+    def long_display
+      old_long_display.sub('Error', RedGreen::Color.yellow('Error'))
+    end
   end
 end
--- redgreen.gemspec
+++ redgreen.gemspec
@@ -1,15 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 Gem::Specification.new do |s|
-  s.name = %q{redgreen}
+  s.name = %q{freegenie-redgreen}
   s.version = "0.1.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Pat Eyler", "Chris Wanstrath", "Luke Pearce"]
   s.date = %q{2009-04-06}
   s.default_executable = %q{rg}
-  s.description = %q{kule-redgreen colourises windows console output for tests.  This version works with ZenTest 4.0.0, autotest, snarl, XP and Vista.  Please see the README file for setup or issues with Snarl.}
-  s.email = %q{github@kulesolutions.com}
+  s.description = %q{freegenie-redgreen colourises windows console output for tests.  This version works with ZenTest 4.0.0, autotest, snarl, XP and Vista.  Please see the README file for setup or issues with Snarl.}
+  s.email = %q{freegenie@gmail.com}
   s.executables = ["rg"]
   s.extra_rdoc_files = [
     "README.markdown"
@@ -24,11 +24,11 @@ Gem::Specification.new do |s|
     "version.yml"
   ]
   s.has_rdoc = true
-  s.homepage = %q{http://github.com/kule/redgreen}
+  s.homepage = %q{http://github.com/freegenie/redgreen}
   s.rdoc_options = ["--charset=UTF-8"]
   s.require_paths = ["lib"]
   s.rubygems_version = %q{1.3.1}
-  s.summary = %q{kule-redgreen colourises windows console output for tests.}
+  s.summary = %q{freegenie-redgreen colourises windows console output for tests.}
 
   if s.respond_to? :specification_version then
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
